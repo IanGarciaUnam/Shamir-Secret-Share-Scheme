4a12f135fde75288a134621a4f54fee0e9761c55
IanGarciaUnam <iangarcia@ciencias.unam.mx>
2021-01-15 12:39:30 -0600

    Adding structure

---
+++ s4/operations.py
@@ -0,0 +1,28 @@
+import getpass
+import hashlib
+
+class operations:
+
+
+	@estaticmethod
+	def input_invisible(show_to_public:str):
+		"""
+		The user can give him password in secret mode
+		Args:
+			show_to_public: str - Password
+		Return getpass.getpass(show_to_public)
+		"""
+		return getpass.getpass(show_to_public)
+
+	def apply_sha_256(content:str):
+		"""
+		Apply SHA-256 to the content of the String
+		Args:
+			content:str - content to be applied by sha-256
+		Return:
+			str: Hexadecimal Character of the content applied to SHA-256
+		"""
+
+		return hashlib.sha256(content.encode())
+
+
