02e61aecbf28757d80cd521c4f29afe252c14c1c
David Hernández Uriostegui <53274521+DavidHdezU@users.noreply.github.com>
2021-01-18 15:36:06 -0600

    Adding files

---
+++ s4/Decrypter.py
@@ -0,0 +1,48 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+from Encrypter import Encrypter
+import hashlib
+
+class Decrypter:
+    """
+    A class that has decrypting methods
+    """
+    def __init__(self, encrypter, cryp_file, key):
+        """
+        Constrcut a decrypter method
+
+        Args:
+            encrypter (Encrypter): We obtain the IV from the original Encrypter
+            cryp_file (str): Encrypted file
+            key (str): password from the Encrypter
+        """
+        self.iv = encrypter.get_cipherIV()
+        self.key = hashlib.sha256(key.encode('utf8')).digest()
+        self.cipher =  AES.new(self.key, AES.MODE_CBC, self.iv)
+        self.file = cryp_file
+        
+    def decipher_file(self):
+        """
+        Decrypts the encrypted file
+
+        Returns:
+            file: Decrypted file
+        """
+        with open(self.file, 'rb') as f:
+            encrypted_file = f.read()
+            
+        decrypted_file = unpad(self.cipher.decrypt(encrypted_file), AES.block_size)
+        
+        return decrypted_file
+    
+    def save_decrypted_file(self, new_name):
+        """
+        Saves the decrypted file
+
+        Args:
+            new_name (str): new name for the the decrypted file
+        """
+        with open(new_name, 'wb') as f:
+            f.write(self.decipher_file())
+            
+            
---
+++ s4/Encrypter.py
@@ -0,0 +1,53 @@
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+import hashlib
+import random
+
+class Encrypter:
+    def __init__(self, in_file, key):
+        """
+        Creates an object that encryptes a file
+
+        Args:
+            in_file (str): file to encrypt
+            key (str): password
+        """
+        self.file = in_file
+        self.key = hashlib.sha256(key.encode('utf8')).digest()
+        self.mode = AES.MODE_CBC
+        self.cipher = AES.new(self.key, self.mode)
+        
+        
+    def encrypt_file(self):
+        """
+        Encrypts the file given
+
+        Returns:
+            file: The file encrypted with AES
+        """
+        with open(self.file, 'rb') as f:
+            orig_file = f.read()
+        
+        
+        return self.cipher.encrypt(pad(orig_file, AES.block_size))
+    
+    def save_encrypted_file(self, out_name):
+        """
+        Saves the encrypted file
+
+        Args:
+            out_name (str): the new name for the encrypted file
+        """
+        with open(out_name, 'wb') as f:
+            f.write(self.encrypt_file())
+            
+    def get_cipherIV(self):
+        """
+        Returns the IV from the cipher
+
+        Returns:
+            list: IV from cipher
+        """
+        return self.cipher.iv
+    
+        
